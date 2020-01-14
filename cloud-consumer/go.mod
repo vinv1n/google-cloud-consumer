bcd2bf8cc1f796f1632b5d0c93fc6bfea7879aa3
Juho Sipola <jusipola@student.oulu.fi>
2020-01-14 23:02:12 +0200

    Add websockets

--- cloud-consumer/go.mod
+++ cloud-consumer/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	cloud.google.com/go v0.49.0
+	github.com/gorilla/websocket v1.4.1
 	google.golang.org/genproto v0.0.0-20191115221424-83cc0476cb11
 )
--- cloud-consumer/main.go
+++ cloud-consumer/main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	websocket "github.com/gorilla/websocket"
 	speech "cloud.google.com/go/speech/apiv1"
 	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
 )
@@ -15,8 +16,6 @@ func main() {
 	http.HandleFunc("/api", handleResponse)
 	http.HandleFunc("/", landingPage)
 
-	// logging is always a plus
-	// TODO add TLS handling
 	fmt.Println(http.ListenAndServe(":8081", nil))
 }
 
@@ -24,65 +23,61 @@ func landingPage(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Server running :)")
 }
 
-func createGoogleCloudRequest(audio []byte, channel chan bool) {
-	/*
-		A value of file should be used as long as possible as it is more efficient
-		in golang that passing a pointer
-	*/
-	err, response := makeRequest(audio)
+func createGoogleCloudRequest(audio []byte, channel chan []byte) {
+	response, err := makeRequest(audio)
 	if err != nil {
 		log.Fatal("Error during request to google api", err)
-		channel <- false
 
-	} else {
-		// TODO add stuff to File structure
-		log.Fatal("success")
-
-		// this could be done better and more elegant manner
-		channel <- true
 	}
-	log.Fatal("Response", response)
+	channel <- response
 	close(channel)
 }
 
-func handleResponse(writer http.ResponseWriter, request *http.Request) {
+func createWebsocket(res []byte) {
+	// creates new websocket
+	conn, _, err := websocket.DefaultDialer.Dial("ws://0.0.0.0:8888", nil)
+	if err != nil {
+		log.Panic("Error during connecting websocket", err)
+	}
+	defer conn.Close()
 
+	err = conn.WriteMessage(websocket.TextMessage, res)
+	if err != nil {
+		log.Panic("Failed send data to the socket", err)
+	}
+	// close the socket
+	conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+}
+
+func handlePost(request *http.Request) {
+	audio, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		log.Panic("Error during reading request content. Error %v", err)
+	}
+
+	channel := make(chan []byte, 100)
+
+	// makes request to the google cloud api
+	go createGoogleCloudRequest(audio, channel)
+	defer createWebsocket(<-channel)
+}
+
+func handleResponse(writer http.ResponseWriter, request *http.Request) {
 	switch request.Method {
 	case "POST":
-		audio, err := ioutil.ReadAll(request.Body)
-		if err != nil {
-			fmt.Fprintf(writer, "Error during reading request content. Error %v", err)
-		}
-		channel := make(chan bool, 1)
-
-		// creates a new go runtime routine
-		go createGoogleCloudRequest(audio, channel)
-
-		// waits untill response is sent to the channel
-		defer func() {
-			// TODO fix the result codes and
-			result := <-channel
-			if result == true {
-				fmt.Fprint(writer, "Success", 200)
-			} else {
-				fmt.Fprint(writer, "Failure", 400)
-			}
-		}()
+		handlePost(request)
 	default:
-		http.Error(writer, "Method not supported", 403) // FIXME the error code should be replaced
+		http.Error(writer, "Method not supported", 403)
 	}
 }
 
-func makeRequest(audio []byte) ([]string, error) {
-	/*
-		Based on example provided on official google cloud docmentation
-		TODO add some better handling for errors
-	*/
+func makeRequest(audio []byte) ([]byte, error) {
+	// Based on example provided on official google cloud documentation
+	// TODO investigate if we could use websockets from mic thingy to stream
+	// audio to cloud
 
-	// base context
+	// create client for google cloud
 	context := context.Background()
-
-	// crate client for google cloud
 	client, err := speech.NewClient(context)
 	if err != nil {
 		log.Fatalf("Could not create client for google cloud, error %v", err)
@@ -102,31 +97,19 @@ func makeRequest(audio []byte) ([]string, error) {
 	if err != nil {
 		log.Fatalf("Failed to recognized audio %s", err)
 		return nil, err
-
 	}
 	results := parseResults(response)
 	return results, nil
 }
 
-// TODO check type and content of response
-// also rethink all of this
-func parseResults(resp *speechpb.RecognizeResponse) []string {
-	count := 0
-	resmap := make([]string, len(resp.GetResults()))
-
-	// HACK: google api returns float32, this is noway near clean and nice solution
-	var confidence float32 = -1.0
-	var word string = ""
+func parseResults(resp *speechpb.RecognizeResponse) []byte {
+	// get the alternative that have hights confidence
+	// FIXME horribly broken
+	var s string;
 	for _, res := range resp.Results {
 		for _, alt := range res.Alternatives {
-			if confidence == -1 || alt.Confidence > confidence {
-				confidence = alt.Confidence
-				word = alt.Transcript
-			}
+			s = alt.Transcript
 		}
-		resmap = append(resmap, word)
-		count += 1
 	}
-	return resmap
-
+	return []byte(s)
 }
